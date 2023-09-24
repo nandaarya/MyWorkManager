4ef13f730f87b419a6473593182c40dc96c62521
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-24 22:48:43 +0700

    Add constraints to work manager (Task will execute when internet connected)

--- app/src/main/java/com/example/myworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/myworkmanager/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.myworkmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -51,8 +49,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val data = Data.Builder()
             .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
             .build()
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
         val oneTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
             .setInputData(data)
+            .setConstraints(constraints)
             .build()
         workManager.enqueue(oneTimeWorkRequest)
         workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
